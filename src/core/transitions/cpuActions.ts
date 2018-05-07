e8714effbf72a36d96bac6d3eec0ee25271d97ff
Unknown <rcosta.atila@gmail.com>
2018-05-07 20:47:19 -0300

    fixed troop distribution by cpu

--- src/core/transitions/cpuActions.ts
+++ src/core/transitions/cpuActions.ts
@@ -7,14 +7,20 @@ export const cpuAction = (nextPhase: GamePhase): void => {
     const { game, country } = store.getState();
     const countries = playerCountries(game.turnOwner, country, 0);
     if (nextPhase === 'DISTRIBUTION') {
-        countries.every((value: string, index: number, array: string[]) => {
-            if (store.getState().player[game.turnOwner].availableTroops > 0) {
-                store.dispatch(incrementTroops(value, 1));
-                return true;
-            } else {
-                return false;
-            }
-        });
+        var availableTroops: number = store.getState().player[game.turnOwner].availableTroops;
+        while (availableTroops > 0) {
+            console.log(availableTroops);
+            countries.every((value: string, index: number, array: string[]) => {
+                if (availableTroops > 0) {
+                    store.dispatch(incrementTroops(value, 1));
+                    availableTroops = availableTroops - 1;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        }
+        
     } else if (nextPhase === 'ATTACK') {
         return;
     } else if (nextPhase === 'MOVE') {
