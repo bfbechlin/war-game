221a80e3c4ce3b4553b3cebe3d1ae31bc1f9210d
Unknown <rcosta.atila@gmail.com>
2018-06-14 22:35:44 -0300

    Add get and set methods for MenuStore

--- src/store/menu/menuStore.ts
+++ src/store/menu/menuStore.ts
@@ -3,15 +3,47 @@ import { observable } from 'mobx';
 class MenuStore {
 
   @observable quantity: number;
-  @observable selectables: String[];
-  @observable selecteds: String[];
-  @observable viewMode: String;
+  @observable selectables: string[];
+  @observable selecteds: string[];
+  @observable viewMode: string;
 
   constructor() {
     this.quantity = 1;
     this.selectables = ['Brazil', 'Peru', 'Venezuela', 'North Africa'];
     this.selecteds = [];
     this.viewMode = 'PLAYER';
+  }
+
+  getQuantity() {
+    return this.quantity;
+  }
+
+  @action setQuantity(quantity: number) {
+    this.quantity = quantity;
+  }
+
+  getSelectables() {
+    return this.selectables;
+  }
+
+  @action setSelectables(selectables: string[]) {
+    this.selectables = selectables;
+  }
+
+  getSelecteds() {
+    return this.selecteds;
+  }
+
+  @action setSelecteds(selecteds: string[]) {
+    this.selecteds = selecteds;
+  }
+
+  getViewMode() {
+    return this.viewMode;
+  }
+
+  @action setViewMode(viewMode: string) {
+    this.viewMode = viewMode;
   }
 
 }
