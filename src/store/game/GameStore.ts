24adc3dfa046ca87ba7c6c9ef8fad418817a82c6
Unknown <rcosta.atila@gmail.com>
2018-06-14 23:30:30 -0300

    Add GameStore properties and methods

--- src/store/game/GameStore.ts
+++ src/store/game/GameStore.ts
@@ -1,6 +1,7 @@
-import { observable } from 'mobx';
-import { GameState } from './types';
+import { observable, action } from 'mobx';
+import { GamePhase } from './types';
 
+/*
 const gameInitState: GameState = {
     round: 1,
     phase: 'INIT',
@@ -10,13 +11,83 @@ const gameInitState: GameState = {
     playerOrder: [],
     cardsBonus: 20,
   };  
+*/
 
 class GameStore {
 
-  @observable state: GameState;
+  @observable round: number;
+  @observable phase: GamePhase;
+  @observable remainingTime: number;
+  @observable turnOwner: string;
+  @observable activePlayers: string[];
+  @observable playerOrder: string[];
+  @observable cardsBonus: number;
 
   constructor() {
-    this.state = gameInitState;
+    this.round = 1;
+    this.phase = 'INIT';
+    this.remainingTime = 0;
+    this.turnOwner = '';
+    this.activePlayers = [];
+    this.playerOrder = [];
+    this.cardsBonus = 20;
+  }
+
+  getCardBonus(): number {
+    return this.cardsBonus;
+  }
+
+  @action setCardBonus(quantity: number) {
+    this.cardsBonus = quantity;
+  }
+
+  @action nextGamePhase() {
+      // TODO: - MIDDLEWARE CODE
+  }
+
+  getGamePhase(): GamePhase {
+    return this.phase;
+  }
+
+  @action setGamePhase(phase: GamePhase) {
+    this.phase = phase;
+    // TODO: - MIDDLEWARE CODE
+  }
+
+  getTurnOwner() {
+    return this.turnOwner;
+  }
+
+  @action setTurnOwner(player: string) {
+    this.turnOwner = player;
+  }
+
+  getRemainingTime(): number {
+    return this.remainingTime;
+  } 
+
+  @action decrementRemainingTime() {
+    this.remainingTime -= 1;
+  }
+
+  @action setRemainingTime(remainingTime: number) {
+    this.remainingTime = remainingTime;
+  }
+
+  getActivePlayers(): string[] {
+    return this.activePlayers;
+  }
+
+  @action setActivePlayers(activePlayers: string[]) {
+    this.activePlayers = activePlayers;
+  }
+
+  getPlayerOrder(): string[] {
+    return this.playerOrder;
+  }
+
+  @action setPlayerOrder(playerOrder: string[]) {
+    this.playerOrder = playerOrder;
   }
 
 }
