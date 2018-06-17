436073a7184705d8c0d17fd65ffe30592d69b69e
Atila Costa <rcosta.atila@gmail.com>
2018-06-17 01:40:22 -0300

    Update store index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,64 +1,31 @@
-import { createStore, applyMiddleware, Store, combineReducers, Reducer, Dispatch } from 'redux';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { History } from 'history';
-import createHistory from 'history/createBrowserHistory';
-import { gamePhaseWatcher, remainingTimeWatcher } from './game/midlewares';
-
-import { CountryState } from './country/types';
-import { MenuState } from './menu/types';
+import playerStore from './player/PlayerStore';
+import gameStore from './game/GameStore';
+import menuStore from './menu/MenuStore';
+import countryStore from './country/CountryStore';
 import { PlayerState } from './player/types';
 import { GameState } from './game/types';
-
-import countryReducer, { countryInitState } from './country/reducer';
-import menuReducer, { initialState as MenuInitState } from './menu/reducer';
-import playerReducer, { playerInitState } from './player/reducer';
-import gameReducer, { gameInitState } from './game/reducer';
+import { MenuState } from './menu/types';
+import { CountryState } from './country/types';
 
 export interface ApplicationState {
   country: CountryState;
   menu: MenuState;
-  player: PlayerState;
+  player: PlayerState; 
   game: GameState;
 }
 
-export const initialState = {
-  country: countryInitState,
-  menu: MenuInitState,
-  player: playerInitState,
-  game: gameInitState,
+export const initialState: ApplicationState = { 
+  country: countryStore.countries, 
+  menu: menuStore.menuState, 
+  player: playerStore.players, 
+  game: gameStore.gameState, 
+}
+
+const store = {
+  game: gameStore,
+  player: playerStore,
+  menu: menuStore,
+  country: countryStore,
 };
 
-export const reducers: Reducer<ApplicationState> = combineReducers<ApplicationState>({
-  router: routerReducer,
-  country: countryReducer,
-  menu: menuReducer,
-  player: playerReducer,
-  game: gameReducer,
-});
-
-// Additional props for connected React components. This prop is passed by default with `connect()`
-export interface ConnectedReduxProps {
-  // Correct types for the `dispatch` prop passed by `react-redux`.
-  // Additional type information is given through generics.
-  dispatch: Dispatch<ApplicationState>;
-}
-
-export function configureStore(
-  history: History,
-): Store<ApplicationState> {
-  const composeEnhancers = composeWithDevTools({});
-
-  return createStore<ApplicationState>(
-    reducers,
-    initialState,
-    composeEnhancers(
-      applyMiddleware(
-        routerMiddleware(history),
-        remainingTimeWatcher,
-        gamePhaseWatcher
-    )),
-  );
-}
-
-export default configureStore(createHistory());
+export default store;
